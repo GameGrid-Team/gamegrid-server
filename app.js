197ac988a83a6d94fc3bbbf737b3161da163e032
woozai <wooozai@gmail.com>
2024-07-17 01:25:23 +0300

    testing shit

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
+const Joi = require('joi')
 const express = require('express')
 const { connectToDb, getDb } = require('./db')
 const app = express()
-
+app.use(express.json())
 // connectToDb((err) => {
 //   if (!err) {
 //     app.listen(3000, () => console.log("Listening on port 3000"));
 //   }
 // });
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening on port ${port}`))
-
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-app.get('/api/users', (req, res) => {
-  res.send([1, 2, 3])
-})
-app.get('/api/users/:id', (req, res) => {
-  res.send(req.params.id)
-})
-app.get('/api/posts/:year/:month', (req, res) => {
-  res.send(req.query)
-})
-
-// Template for users db
-
 users = [
   {
-    user_id: '123',
+    user_id: '1',
     nickname: 'naruto',
     fisrt_name: 'naruto',
     last_name: 'uzumaki',
@@ -39,25 +22,54 @@ users = [
     interest: ['hokage', 'shipuden'],
   },
   {
-    user_id: '456',
+    user_id: '2',
     nickname: 'Tobi',
     fisrt_name: 'madara',
     last_name: 'uchiha',
     password: '11223344',
     gender: 'male',
     email: 'madara@gmail.com',
-    birthday: 5,
+    birthday: 50,
     interest: ['tzkuyomi', 'king'],
   },
-  {
-    user_id: '789',
-    nickname: 'usless',
-    fisrt_name: 'sakura',
-    last_name: 'Haruno',
-    password: '11223344',
-    gender: 'female',
-    email: 'sakura@gmail.com',
-    birthday: 5,
-    interest: ['nothing', 'boring'],
-  },
 ]
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+app.get('/api/users', (req, res) => {
+  res.send(users)
+})
+app.get('/api/users/:user_id', (req, res) => {
+  const user = users.find((u) => u.user_id === req.params.user_id.toString())
+  if (!user) res.status(404).send('User with given user_id was not found')
+  res.send(user)
+})
+
+app.post('/api/users', (req, res) => {
+  //   const schema = {
+  //     name: Joi.string().min(3).required(),
+  //   }
+  //   const result = Joi.valid(req.body, schema)
+  //   if (result.error) {
+  //     res.status(400).send(result.error)
+  //     return
+  //   }
+
+  if (!req.body.nickname || req.body.nickname.length < 3) {
+    res.status(400).send('Name is required be min of 3 characters')
+    return
+  } // its only a testttttttt dont wory :)
+
+  const user = {
+    user_id: users.length + 1,
+    nickname: req.body.nickname,
+  }
+  users.push(user)
+  res.send(user)
+})
+
+// Template for users db
