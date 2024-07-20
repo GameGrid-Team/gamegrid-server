d1ce5e93b268d7eeb956a47ccce6e5d395170eb6
woozai <wooozai@gmail.com>
2024-07-20 14:11:02 +0300

    added api

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const Joi = require('joi')
+var cors = require('cors')
+
 const { connectToDb, getDb } = require('./db')
 const { ObjectId } = require('mongodb')
+
 const app = express()
 app.use(express.json())
 const generalTexts = require('./fixture/general_text')
 let db
 
+app.use(cors())
+
 const port = process.env.PORT || 3001
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
-app.get('/about', (req, res) => {
+app.get('/api/about', (req, res) => {
   res.send([generalTexts.aboutTxt])
 })
-app.get('/ping', (req, res) => {
-  res.send('pong I LOVE U')
+app.get('/api/ping', (req, res) => {
+  res.send(['pong I LOVE U'])
 })
