9509e59ee97d44407515c75dadfacc04661ac3ab
woozai <wooozai@gmail.com>
2024-07-20 19:06:15 +0300

    removed test

--- cypress/e2e/general_text.js
+++ cypress/e2e/general_text.js
@@ -1,6 +1,3 @@
-
-
-
 let aboutTxt = `About Page Content for GameGrid
 About GameGrid
 Welcome to GameGrid (GG), the ultimate social media platform designed specifically for gamers! Our mission is to create a vibrant and engaging community where gamers from all around the world can connect, share, and compete. With features tailored to enhance the gaming experience, GameGrid is more than just a social network; it's a hub for gamers to thrive.
@@ -25,14 +22,6 @@ Join us on this exciting journey and be a part of the GameGrid community!
 
 Feel free to modify any section to better suit your team's needs or to add more information about your project.`
 
-
-
-
-
-
-
-
 module.exports = {
-    aboutTxt,
-  };
-  
+  aboutTxt,
+}
