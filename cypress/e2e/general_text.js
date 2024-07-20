50fb908ab62e2fac9f0a82b2f6181ad14582450a
woozai <wooozai@gmail.com>
2024-07-20 18:48:00 +0300

    trying stuff

--- cypress/e2e/general_text.js
+++ cypress/e2e/general_text.js
@@ -1,6 +1,7 @@
-describe('About page', () => {
-  it('if the about text is correct', () => {
-    const testText = `About Page Content for GameGrid
+
+
+
+let aboutTxt = `About Page Content for GameGrid
 About GameGrid
 Welcome to GameGrid (GG), the ultimate social media platform designed specifically for gamers! Our mission is to create a vibrant and engaging community where gamers from all around the world can connect, share, and compete. With features tailored to enhance the gaming experience, GameGrid is more than just a social network; it's a hub for gamers to thrive.
 
@@ -24,10 +25,14 @@ Join us on this exciting journey and be a part of the GameGrid community!
 
 Feel free to modify any section to better suit your team's needs or to add more information about your project.`
 
-    cy.request('/api/about')
-      .its('body')
-      .then((body) => {
-        expect(body.aboutText).to.be.equal(testText)
-      })
-  })
-})
+
+
+
+
+
+
+
+module.exports = {
+    aboutTxt,
+  };
+  
