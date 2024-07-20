d4e8dbe32b6fe4bd79cb76592930114ed0aea8bc
kobia77 <kobialen77@gmail.com>
2024-07-20 18:10:08 +0300

    nothing

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const Joi = require('joi')
-var cors = require('cors')
 
 const { connectToDb, getDb } = require('./db')
 const { ObjectId } = require('mongodb')
@@ -10,14 +9,26 @@ app.use(express.json())
 const generalTexts = require('./fixture/general_text')
 let db
 
-app.use(cors())
 
 const port = process.env.PORT || 3001
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
 app.get('/api/about', (req, res) => {
-  res.send([generalTexts.aboutTxt])
+  res.json([generalTexts.aboutTxt])
 })
 app.get('/api/ping', (req, res) => {
-  res.send(['pong I LOVE U'])
+  res.json(['pong I LOVE U'])
+})
+
+
+app.post('/api/users', (req, res) => {
+  if (!req.body.email) {
+    res.status(400).json({"error":"nickname is empty!" })
+    return
+  }
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  }
+  res.status(200).json(user)
 })
