f7fc19c2219a71560ab2964ac0842e9fcf62de73
woozai <wooozai@gmail.com>
2024-07-23 14:08:38 +0300

    added database call

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const Joi = require('joi')
 const cors = require('cors')
-// const { connectToDb, getDb } = require('./db')
-// const { ObjectId } = require('mongodb')
+const { connectToDb, getDb } = require('./db')
+const { ObjectId } = require('mongodb')
 const app = express()
 app.use(express.json())
 const generalTexts = require('./fixture/general_text')
-// let db
+let db
 
-const port = process.env.PORT || 3001
-app.listen(port, () => console.log(`Listening on port ${port}`))
+connectToDb((err) => {
+  if (!err) {
+    const port = process.env.PORT || 3001
+    app.listen(port, () => console.log(`Listening on port ${port}`))
+    db = getDb()
+  }
+})
+// const port = process.env.PORT || 3001
+// app.listen(port, () => console.log(`Listening on port ${port}`))
 
 app.use(cors())
+
+app.get('/api/users', (req, res) => {
+  let users = []
+  db.collection('users')
+    .find()
+    .ni.forEach((user) => {
+      users.push(user)
+      if (user.nickname === 'naruto') console.log('User:::::: ', user)
+    })
+    .then(() => {
+      res.status(200).json(users)
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Couldn't find users from list" })
+    })
+})
 
 app.get('/api/about', (req, res) => {
   res.json({ aboutText: generalTexts.aboutTxt })
